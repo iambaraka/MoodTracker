052f1ddcfaef1781a01edf9aae7e06392c37bd50
Baraka M Mulumia <65751779+barakamulumia@users.noreply.github.com>
2023-01-19 19:30:52 +0300

    feat: :sparkles: Persisting data and Async storage

--- src/providers/App.provider.tsx
+++ src/providers/App.provider.tsx
@@ -4,9 +4,37 @@ import {
   createContext,
   useCallback,
   useContext,
+  useEffect,
   useState,
 } from 'react';
 import { MoodOptionType, MoodOptionWithTimestamp } from '../types';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+type AppData = {
+  moodList: MoodOptionWithTimestamp[];
+};
+
+const APP_DATA_KEY = 'Mood-TRACKER-APP-DATA';
+
+const setAppData = async (appData: AppData): Promise<void> => {
+  try {
+    const jsonValue = JSON.stringify(appData);
+    await AsyncStorage.setItem(APP_DATA_KEY, jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+const getAppData = async (): Promise<AppData> => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(APP_DATA_KEY);
+    return jsonValue != null ? JSON.parse(jsonValue) : { moodList: [] };
+  } catch (e) {
+    // error reading value
+    return { moodList: [] };
+  }
+};
 
 type AppContextType = {
   greeting: string;
@@ -27,13 +55,28 @@ export const AppProvider: FunctionComponent<PropsWithChildren<{}>> = ({
   const [moodList, setMoodList] = useState<MoodOptionWithTimestamp[]>([]);
 
   const handleSelectMood = useCallback((mood: MoodOptionType) => {
-    setMoodList((prevMoodList) => [
-      ...prevMoodList,
-      {
-        mood,
-        timestamp: Date.now(),
-      },
-    ]);
+    setMoodList((prevMoodList) => {
+      const newMoodList = [
+        ...prevMoodList,
+        {
+          mood,
+          timestamp: Date.now(),
+        },
+      ];
+
+      setAppData({ moodList: newMoodList });
+
+      return newMoodList;
+    });
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const appData = await getAppData();
+      setMoodList(appData.moodList);
+    };
+
+    fetchData();
   }, []);
 
   return (
