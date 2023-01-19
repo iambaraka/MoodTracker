dc87150f0e55ba7649b31fe55d1fe59eeceae67c
Baraka M Mulumia <65751779+barakamulumia@users.noreply.github.com>
2023-01-19 20:03:45 +0300

    feat: :construction: Add network images

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,15 +1,20 @@
-import { StyleSheet, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, View } from 'react-native';
 
 import { FunctionComponent } from 'react';
 import { MoodPicker } from '../components/MoodPicker';
 import { useAppContext } from '../providers/App.provider';
+
+const imageUrl =
+  'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
 
 export const Home: FunctionComponent = () => {
   const { handleSelectMood } = useAppContext();
 
   return (
     <View style={styles.container}>
-      <MoodPicker handleSelectMood={handleSelectMood} />
+      <ImageBackground source={{ uri: imageUrl }} style={styles.img}>
+        <MoodPicker handleSelectMood={handleSelectMood} />
+      </ImageBackground>
     </View>
   );
 };
@@ -17,6 +22,11 @@ export const Home: FunctionComponent = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+  },
+  img: {
+    flex: 1,
+    resizeMode: 'cover',
     justifyContent: 'center',
   },
 });
